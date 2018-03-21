acf4fbfb23096616b86c4c79adcc2a85825e76db
Andreu Conesa Simo <andreu.conesa@atmira.com>
2018-03-21 20:04:11 +0100

    Agregar archivos de proyecto.

---
+++ CalculadoraIntegrationbTests/CalculadoraIntegrationTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PooVueling;
+
+namespace CalculadoraIntegrationbTests
+{
+    [TestClass]
+    public class CalculadoraIntegrationTests
+    {
+        [TestMethod]
+        public void TestSuma()
+        {
+            int expected = 5;
+            ICalculadora calculadora = new Calculadora();
+            int actual = calculadora.Suma(2,3);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestResta()
+        {
+            int expected = 5;
+            ICalculadora calculadora = new Calculadora();
+            int actual = calculadora.Suma(2, 3);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMultiplicacion()
+        {
+            Assert.Fail();
+        }
+        [TestMethod]
+        public void TestDivision()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ PooVuelingTests/CalculadoraIntegrationTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PooVueling.Tests
+{
+    [TestClass()]
+    public class CalculadoraIntegrationTests
+    {
+        ICalculadora iCalculadora = new Calculadora();
+
+        [TestMethod()]
+        public void DivisionTest()
+        {
+            Assert.IsTrue(iCalculadora.Division(6,3) == 2);
+        }
+
+        [TestMethod()]
+        public void MultiplicacionTest()
+        {
+            Assert.IsTrue(iCalculadora.Multiplicacion(2, 3) == 6);
+        }
+
+        [TestMethod()]
+        public void RestaTest()
+        {
+            int expected = 1;
+            int actual = iCalculadora.Resta(3, 2);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void SumaTest()
+        {
+            //int expected = 5;                      
+            //int actual = iCalculadora.Suma(2,3);
+            //Assert.AreEqual(expected, actual);
+            Assert.IsTrue(iCalculadora.Suma(2, 3) == 5);
+        }
+    }
+}
