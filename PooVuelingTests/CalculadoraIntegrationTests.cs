065beff872ddef0bf913a6be0affd3f893b63e4e
Andreu Conesa Simo <andreu.conesa@est.fib.upc.edu>
2018-03-23 10:27:23 +0100

    Changed test method for datatest method. Using parametrized data.

--- PooVuelingTests/CalculadoraIntegrationTests.cs
+++ PooVuelingTests/CalculadoraIntegrationTests.cs
@@ -10,33 +10,38 @@ namespace PooVueling.Tests
         //[TestMethod()]
         [DataRow(4,2,2)] // cada data row es una casuistica
         [DataRow(9,3,3)] // 9/3 = 3
-        [DataTestMethod] // permite enviar datos al método de test
+        [DataTestMethod] // permite enviar datos al método de test (parametrizar)
         public void DivisionTest(int num1, int num2, int resultado)
         {
             Assert.IsTrue(iCalculadora.Division(num1,num2) == resultado);
         }
 
-        [TestMethod()]
-        public void MultiplicacionTest()
+        [DataRow(2,3,6)]
+        [DataRow(5,7,35)]
+        [DataTestMethod]
+        public void MultiplicacionTest(int num1, int num2, int resultado)
         {
-            Assert.IsTrue(iCalculadora.Multiplicacion(2, 3) == 6);
+            Assert.IsTrue(iCalculadora.Multiplicacion(num1, num2) == resultado);
         }
 
-        [TestMethod()]
-        public void RestaTest()
-        {
-            int expected = 1;
-            int actual = iCalculadora.Resta(3, 2);
-            Assert.AreEqual(expected, actual);
+        [DataRow(34,8,26)]
+        [DataRow(50,29,21)]
+        [DataTestMethod]
+        public void RestaTest(int num1, int num2, int resultado)
+        {            
+            int actual = iCalculadora.Resta(num1, num2);
+            Assert.AreEqual(resultado, actual);
         }
 
-        [TestMethod()]
-        public void SumaTest()
+        [DataRow(45,53,98)]
+        [DataRow(87,35,122)]
+        [DataTestMethod]
+        public void SumaTest(int num1, int num2, int resultado)
         {
             //int expected = 5;                      
             //int actual = iCalculadora.Suma(2,3);
             //Assert.AreEqual(expected, actual);
-            Assert.IsTrue(iCalculadora.Suma(2, 3) == 5);
+            Assert.IsTrue(iCalculadora.Suma(num1, num2) == resultado);
         }
     }
 }
